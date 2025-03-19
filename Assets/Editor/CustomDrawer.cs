0b18e45ebd45d0a53dbe01cff08ab77f96d2fd1a
Eray <eray.yasar37@hotmail.com>
2025-03-19 16:35:18 +0300

    Change customdrawer to draw cell and piece seperately

--- Assets/Editor/CustomDrawer.cs
+++ Assets/Editor/CustomDrawer.cs
@@ -1,9 +1,6 @@
-using GridRelated;
 using UnityEditor;
 using UnityEngine;
-using System;
-using System.Linq;
-using Misc;
+using GridRelated;
 
 namespace Editor
 {
@@ -12,67 +9,73 @@ namespace Editor
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            // Draw the label
             EditorGUI.PrefixLabel(position, label);
 
-            // Retrieve properties
             var rowsProperty = property.FindPropertyRelative("height");
             var columnsProperty = property.FindPropertyRelative("width");
-            var dataProperty = property.FindPropertyRelative("data");
+            var dataProperty = property.FindPropertyRelative("cellData");
 
-            // Adjust position for fields
             position.y += EditorGUIUtility.singleLineHeight;
 
-            // Draw row and column fields
             rowsProperty.intValue = EditorGUI.IntField(
                 new Rect(position.x, position.y, position.width / 2 - 5, EditorGUIUtility.singleLineHeight),
                 "Rows", rowsProperty.intValue);
-            
+
             columnsProperty.intValue = EditorGUI.IntField(
-                new Rect(position.x + position.width / 2 + 5, position.y, position.width / 2 - 5, EditorGUIUtility.singleLineHeight),
+                new Rect(position.x + position.width / 2 + 5, position.y, position.width / 2 - 5,
+                    EditorGUIUtility.singleLineHeight),
                 "Columns", columnsProperty.intValue);
 
-            // Update array size if rows/columns change
             int totalSize = Mathf.Max(1, rowsProperty.intValue * columnsProperty.intValue);
+
             if (dataProperty.arraySize != totalSize)
             {
                 dataProperty.arraySize = totalSize;
+                for (int i = 0; i < totalSize; i++)
+                {
+                    SerializedProperty cellProperty = dataProperty.GetArrayElementAtIndex(i);
+                    if (cellProperty.managedReferenceValue == null)
+                    {
+                        cellProperty.managedReferenceValue = new CustomGridCell();
+                    }
+                }
             }
 
-            // Prepare enum data for drawing popups
-            CellType[] cellTypes = (CellType[])Enum.GetValues(typeof(CellType));
-            string[] displayNames = cellTypes.Select(ct => ct.ToString()).ToArray();
-            int[] values = cellTypes.Select(ct => (int)ct).ToArray();
-
-            // Draw the grid
             position.y += EditorGUIUtility.singleLineHeight + 5;
-            float cellWidth = 100;
-            for (int row = rowsProperty.intValue - 1; row >= 0; row--) // Reverse row iteration
+            float cellWidth = 120;
+            float cellHeight = EditorGUIUtility.singleLineHeight * 2 + 5;
+
+            float startY = position.y + (rowsProperty.intValue - 1) * cellHeight; // Start from the bottom
+
+            for (int row = 0; row < rowsProperty.intValue; row++) // Start from 0, move up
             {
+                float rowY = startY - row * cellHeight; // Move upwards instead of downwards
+
                 for (int col = 0; col < columnsProperty.intValue; col++)
                 {
                     int index = row * columnsProperty.intValue + col;
-                    SerializedProperty element = dataProperty.GetArrayElementAtIndex(index);
+                    SerializedProperty cellProperty = dataProperty.GetArrayElementAtIndex(index);
+                    SerializedProperty cellTypeProperty = cellProperty.FindPropertyRelative("cellType");
+                    SerializedProperty pieceTypeProperty = cellProperty.FindPropertyRelative("pieceType");
 
-                    // Draw each cell as a popup for the enum
-                    Rect cellRect = new Rect(position.x + col * cellWidth, position.y, cellWidth - 5, EditorGUIUtility.singleLineHeight);
-                    
-                    // Fix: Use intValue instead of enumValueIndex
-                    int currentValue = element.intValue;
-                    int selectedIndex = Array.IndexOf(values, currentValue);
-                    if (selectedIndex == -1) selectedIndex = 0; // Default to first item if not found
-                    
-                    int newIndex = EditorGUI.Popup(cellRect, selectedIndex, displayNames);
-                    element.intValue = values[newIndex]; // Assign corresponding int value
+                    float cellX = position.x + col * cellWidth;
+
+                    // Draw cell type
+                    Rect cellRect = new Rect(cellX, rowY, cellWidth - 5, EditorGUIUtility.singleLineHeight);
+                    EditorGUI.PropertyField(cellRect, cellTypeProperty, GUIContent.none);
+
+                    // Draw piece type below it
+                    Rect pieceRect = new Rect(cellX, rowY + EditorGUIUtility.singleLineHeight, cellWidth - 5,
+                        EditorGUIUtility.singleLineHeight);
+                    EditorGUI.PropertyField(pieceRect, pieceTypeProperty, GUIContent.none);
                 }
-                position.y += EditorGUIUtility.singleLineHeight + 5;
             }
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             var rowsProperty = property.FindPropertyRelative("height");
-            return EditorGUIUtility.singleLineHeight * (rowsProperty.intValue + 3);
+            return EditorGUIUtility.singleLineHeight * (rowsProperty.intValue * 2 + 3);
         }
     }
 }
