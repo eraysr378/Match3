37d396f881f5513ef91b28956cb933369871d8de
Eray <eray.yasar37@hotmail.com>
2025-03-25 14:26:17 +0300

    Split logic into seperate classes

--- Assets/Scripts/Managers/MatchManager.cs
+++ Assets/Scripts/Managers/MatchManager.cs
@@ -1,7 +1,6 @@
-using System.Collections.Generic;
+using GridRelated;
+using MatchSystem;
 using Pieces;
-using Interfaces;
-using Misc;
 using UnityEngine;
 using Grid = GridRelated.Grid;
 
@@ -10,150 +9,61 @@ namespace Managers
     public class MatchManager : MonoBehaviour
     {
         private Grid _grid;
+        private MatchFinder _matchFinder;
+        private MatchHandler _matchHandler;
+        private SwapFinder _swapFinder;
 
-        public void Initialize(Grid grid)
-        {
-            _grid = grid;
-        }
+      
 
         public void OnEnable()
         {
-            EventManager.OnFillCompleted += OnFillCompleted;
+            GridInitializer.OnGridInitialized += Initialize;
+            FillManager.OnFillCompleted += OnFillCompleted;
         }
 
         public void OnDisable()
         {
-            EventManager.OnFillCompleted -= OnFillCompleted;
+            GridInitializer.OnGridInitialized -= Initialize;
+            FillManager.OnFillCompleted -= OnFillCompleted;
+        }
+        private void Initialize(Grid grid)
+        {
+            _grid = grid;
+            _matchFinder = new MatchFinder(grid);
+            _matchHandler = new MatchHandler();
+            _swapFinder = new SwapFinder(grid, _matchFinder);
         }
 
         private void OnFillCompleted()
         {
-            if (ClearAllValidMatches())
-            {
-                EventManager.OnValidMatchCleared?.Invoke();
-            }   
+            FindAndHandleAllMatches();
         }
 
-        private void HandleMatch(List<Piece> matchList)
+        public bool TryGetMatchFormingSwap(out (Piece,Piece)? pieces)
         {
-            foreach (var match in matchList)
-            {
-                match.DestroyPiece();
-            }
+            _swapFinder.TryFindValidSwapForMatch(out pieces);
+            return pieces != null;
         }
-        public bool ClearAllValidMatches()
+        public bool TryHandleMatches(params Piece[] pieces)
         {
-            HashSet<Piece> matchedPieces = new HashSet<Piece>();
+            var matches = _matchFinder.FindMatches(pieces);
 
-            for (int row = 0; row < _grid.Height; row++)
-            {
-                for (int col = 0; col < _grid.Width; col++)
-                {
-                    Piece piece = _grid.GetCell(row, col).CurrentPiece;
-                    if (piece is not IMatchable || matchedPieces.Contains(piece)) continue;
+            if (matches.Count == 0)
+                return false;
 
-                    var matchList = GetMatch(piece);
-                    if (matchList == null) continue;
-                    matchedPieces.UnionWith(matchList);
-                    HandleMatch(matchList);
-                }
-            }
-
-            return matchedPieces.Count > 0;
+            _matchHandler.HandleMatches(matches);
+            return true;
         }
 
-        public List<Piece> GetMatch(Piece piece)
+        private void FindAndHandleAllMatches()
         {
-            if (piece is not IMatchable) return null;
+            var allPieces = _grid.GetAllPieces();
+            var matches = _matchFinder.FindMatches(allPieces);
 
-            List<Piece> horizontalPieces = GetHorizontalMatch(piece);
-            if (horizontalPieces.Count >= 2)
+            if (matches.Count > 0)
             {
-                horizontalPieces.Add(piece);
-                foreach (var horizontalPiece in horizontalPieces)
-                {
-                    var verticalExtension = GetVerticalMatch(horizontalPiece);
-                    if (verticalExtension.Count >= 2)
-                    {
-                        horizontalPieces.AddRange(verticalExtension);
-                        break;
-                    }
-                }
+                _matchHandler.HandleMatches(matches);
             }
-
-            if (horizontalPieces.Count >= 3)
-                return horizontalPieces;
-
-
-            List<Piece> verticalPieces = GetVerticalMatch(piece);
-
-            if (verticalPieces.Count >= 2)
-            {
-                verticalPieces.Add(piece);
-                foreach (var verticalPiece in verticalPieces)
-                {
-                    var horizontalExtension = GetHorizontalMatch(verticalPiece);
-                    if (horizontalExtension.Count >= 2)
-                    {
-                        verticalPieces.AddRange(horizontalExtension);
-                        break;
-                    }
-                }
-            }
-
-            return verticalPieces.Count >= 3 ? verticalPieces : null;
-        }
-
-        private List<Piece> GetVerticalMatch(Piece piece)
-        {
-            if (piece is not IMatchable) return new List<Piece>();
-
-            List<Piece> verticalPieces = new List<Piece>();
-            PieceType pieceType = piece.GetPieceType();
-            for (int row = piece.Row - 1; row >= 0; row--)
-            {
-                Piece pieceBelow = _grid.GetCell(row, piece.Col).CurrentPiece;
-                if (pieceBelow is not IMatchable || pieceBelow.GetPieceType() != pieceType)
-                    break;
-
-                verticalPieces.Add(pieceBelow);
-            }
-
-            for (int row = piece.Row + 1; row < _grid.Height; row++)
-            {
-                Piece pieceAbove = _grid.GetCell(row, piece.Col).CurrentPiece;
-                if (pieceAbove is not IMatchable || pieceAbove.GetPieceType() != pieceType)
-                    break;
-
-                verticalPieces.Add(pieceAbove);
-            }
-
-            return verticalPieces;
-        }
-
-        private List<Piece> GetHorizontalMatch(Piece piece)
-        {
-            if (piece is not IMatchable) return new List<Piece>();
-
-            List<Piece> horizontalPieces = new List<Piece>();
-            PieceType pieceType = piece.GetPieceType();
-            for (int col = piece.Col - 1; col >= 0; col--)
-            {
-                Piece pieceLeft = _grid.GetCell(piece.Row, col).CurrentPiece;
-                if (pieceLeft is not IMatchable || pieceLeft.GetPieceType() != pieceType)
-                    break;
-                horizontalPieces.Add(pieceLeft);
-            }
-
-            for (int col = piece.Col + 1; col < _grid.Width; col++)
-            {
-                Piece pieceRight = _grid.GetCell(piece.Row, col).CurrentPiece;
-                if (pieceRight is not IMatchable || pieceRight.GetPieceType() != pieceType)
-                    break;
-                horizontalPieces.Add(pieceRight);
-            }
-
-            return horizontalPieces;
         }
     }
 }
