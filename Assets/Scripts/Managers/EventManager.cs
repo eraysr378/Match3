4f48a1c75d93c320914934b578a42c1600bd5b6f
Eray <eray.yasar37@hotmail.com>
2025-03-25 14:27:40 +0300

    Move some events to their classes

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -1,28 +1,21 @@
 using System;
-using System.Collections.Generic;
 using Cells;
+using Interfaces;
 using Pieces;
 using Misc;
-using UnityEngine;
-using Grid = GridRelated.Grid;
-
 namespace Managers
 {
     public static class EventManager
     {
-        public static Action<Piece> OnPointerDownPiece;
-        public static Action<Piece> OnPointerUpPiece;
-        public static Action<Piece> OnPointerEnterPiece;
-        public static Action<Piece> OnPointerClickedPiece;
-        public static Action<Grid> OnGridInitialized;
-        public static Func<PieceType,int,int,Piece> OnRequestPieceSpawn;
-        public static Func<CellType,int,int,Cell> OnRequestCellSpawn;
-        public static Func<int,int,Piece> OnRequestRandomNormalPieceSpawn;
-        public static Action OnFillCompleted;
-        public static Action OnValidMatchCleared;
-        public static Action<Piece,Piece> OnSwapCompleted;
+        public static Action<Piece,Piece> OnSwapRequested;
+        public static Action<Piece,Piece> OnInstantSwapRequested;
+        
+        public static Func<PieceType,int,int,Piece> OnPieceSpawnRequested;
+        public static Func<CellType,int,int,Cell> OnCellSpawnRequested;
+        public static Func<int,int,Piece> OnRandomNormalPieceSpawnRequested;
+        
         public static Action<Piece> OnPieceReturnToPool;
-
-
+        
+        public static Action<IActivatable> OnPieceActivated;
     }
 }
