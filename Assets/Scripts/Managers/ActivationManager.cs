4d87d4cbed97e2f0ccabc5ed4a7e6f06dde8494e
Eray <eray.yasar37@hotmail.com>
2025-03-25 14:28:48 +0300

    Make activation manager control activations

--- Assets/Scripts/Managers/ActivationManager.cs
+++ Assets/Scripts/Managers/ActivationManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Pieces;
 using Interfaces;
@@ -7,25 +8,59 @@ namespace Managers
 {
     public class ActivationManager : MonoBehaviour
     {
-        public event Action<Piece> OnPieceActivated;
+        public static event Action OnActivationsStarted;
+        public static event Action OnActivationsCompleted;
+        [SerializeField] private int _activeCount = 0;
+        private List<IActivatable> _activePieces = new List<IActivatable>();
+
+
+        private void OnEnable()
+        {
+            EventManager.OnPieceActivated += OnPieceActivated;
+        }
+
+        private void OnDisable()
+        {
+            EventManager.OnPieceActivated -= OnPieceActivated;
+        }
+
+
+        private void OnPieceActivated(IActivatable activatable)
+        {
+            if (_activeCount == 0)
+            {
+                OnActivationsStarted?.Invoke();
+                // Debug.Log("Activation started");    
+            }
+            _activePieces.Add(activatable);
+            _activeCount++;
+            activatable.OnActivationCompleted += HandleActivationCompleted;
+        }
+
+        private void HandleActivationCompleted(IActivatable activatable)
+        {
+            activatable.OnActivationCompleted -= HandleActivationCompleted;
+
+            _activeCount--;
+
+            if (_activeCount == 0) // All activations completed
+            {
+                _activePieces.Clear();
+                OnActivationsCompleted?.Invoke();
+                // Debug.Log("Activation end");    
+
+            }
+        }
 
         public bool TryActivatePiece(Piece piece)
         {
             if (piece is IActivatable activatable)
             {
                 activatable.Activate();
-                OnPieceActivated?.Invoke(piece);
                 return true;
             }
+
             return false;
         }
-
-        // public void ActivatePieces(params Piece[] pieces)
-        // {
-        //     foreach (var piece in pieces)
-        //     {
-        //         TryActivatePiece(piece);
-        //     }
-        // }
     }
 }
