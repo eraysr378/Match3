5624687350dae8c8afb02c284ad84eef78ab7bc8
Eray <eray.yasar37@hotmail.com>
2025-03-25 14:23:55 +0300

    Change file location

--- Assets/Scripts/ResponsiveUI/SafeAreaAdjuster.cs
+++ Assets/Scripts/ResponsiveUI/SafeAreaAdjuster.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
 
-public class SafeAreaAdjuster : MonoBehaviour
+namespace ResponsiveUI
 {
-    private RectTransform _rectTransform;
-    void Awake(){
-        _rectTransform = GetComponent<RectTransform>();
-
-    }
-    void Start()
+    public class SafeAreaAdjuster : MonoBehaviour
     {
-        if(_rectTransform == null){
-            Debug.LogWarning("buuu");
+        private RectTransform _rectTransform;
+        void Awake(){
+            _rectTransform = GetComponent<RectTransform>();
+
         }
-        ApplySafeArea();
-    }
-
-    void ApplySafeArea()
-    {
-        // Get the safe area rectangle
-        Rect safeArea = Screen.safeArea;
-
-        // Convert the safe area rectangle into anchor values
-        Vector2 anchorMin = safeArea.position;
-        Vector2 anchorMax = safeArea.position + safeArea.size;
-
-        // Normalize values to the range [0,1]
-        anchorMin.x /= Screen.width;
-        anchorMin.y /= Screen.height;
-        anchorMax.x /= Screen.width;
-        anchorMax.y /= Screen.height;
-
-        // Apply the normalized anchors to the RectTransform
-        if(_rectTransform == null){
-            return;
+        void Start()
+        {
+            if(_rectTransform == null){
+                Debug.LogWarning("buuu");
+            }
+            ApplySafeArea();
         }
-        _rectTransform.anchorMin = anchorMin;
-        _rectTransform.anchorMax = anchorMax;
-    }
 
-    void OnRectTransformDimensionsChange()
-    {
-        ApplySafeArea(); // Reapply safe area in case of screen changes
+        void ApplySafeArea()
+        {
+            // Get the safe area rectangle
+            Rect safeArea = Screen.safeArea;
+
+            // Convert the safe area rectangle into anchor values
+            Vector2 anchorMin = safeArea.position;
+            Vector2 anchorMax = safeArea.position + safeArea.size;
+
+            // Normalize values to the range [0,1]
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+
+            // Apply the normalized anchors to the RectTransform
+            if(_rectTransform == null){
+                return;
+            }
+            _rectTransform.anchorMin = anchorMin;
+            _rectTransform.anchorMax = anchorMax;
+        }
+
+        void OnRectTransformDimensionsChange()
+        {
+            ApplySafeArea(); // Reapply safe area in case of screen changes
+        }
     }
 }
