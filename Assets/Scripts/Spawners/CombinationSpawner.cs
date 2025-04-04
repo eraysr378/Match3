e1b27a27d25ad9915727da283dada54deb6a7845
Eray <eray.yasar37@hotmail.com>
2025-04-04 18:11:27 +0300

    Add combination spawner

--- Assets/Scripts/Spawners/CombinationSpawner.cs
+++ Assets/Scripts/Spawners/CombinationSpawner.cs
@@ -1,7 +1,36 @@
+using Combinations;
+using Factories.CombinationFactories;
+using GridRelated;
+using Managers;
+using Misc;
+using UnityEngine;
+
 namespace Spawners
 {
-    public class CombinationSpawner
+    public class CombinationSpawner : MonoBehaviour
     {
-        
+        [SerializeField] private GeneralCombinationFactory combinationFactory;
+        public void OnEnable()
+        {
+            EventManager.OnCombinationSpawnRequested += SpawnCombination;
+        }
+
+        private void OnDisable()
+        {
+            EventManager.OnCombinationSpawnRequested -= SpawnCombination;
+        }
+
+ 
+        private Combination SpawnCombination(CombinationType combinationType, int row, int col)
+        {
+            Combination combination = combinationFactory.CreateCombinationBasedOnType(combinationType);
+            if (combination == null) return null;
+            
+            Vector2 pos = GridUtility.GridPositionToWorldPosition(row, col);
+            
+            combination.transform.position = pos;
+            
+            return combination;
+        }
     }
 }
