6b83565a8f414b2d544c559060891518a223b4eb
Eray <eray.yasar37@hotmail.com>
2025-04-04 18:06:39 +0300

    Add combination ruleset

--- Assets/Scripts/CombinationSystem/CombinationRuleSet.cs
+++ Assets/Scripts/CombinationSystem/CombinationRuleSet.cs
@@ -1,32 +1,37 @@
 using System.Collections.Generic;
 using Misc;
-using UnityEngine;
 
-namespace Pieces
+namespace CombinationSystem
 {
     public class CombinationRuleSet
     {
-        private readonly Dictionary<(PieceType, PieceType), PieceType> _combinationRules = new();
+        private readonly Dictionary<(PieceType, PieceType), CombinationType> _combinationRules = new();
 
         public CombinationRuleSet()
         {
-            AddRule(PieceType.RocketPiece, PieceType.BombPiece, PieceType.RocketBombCombination);
+            AddRule(PieceType.RocketPiece, PieceType.BombPiece, CombinationType.RocketBombCombination);
             
-            AddRule(PieceType.BombPiece, PieceType.BombPiece, PieceType.BombBombCombination);
+            AddRule(PieceType.BombPiece, PieceType.BombPiece, CombinationType.BombBombCombination);
             
-            AddRule(PieceType.RocketPiece, PieceType.RainbowPiece, PieceType.RocketBombCombination);
+            AddRule(PieceType.RocketPiece, PieceType.RainbowPiece, CombinationType.RocketBombCombination);
             
-            AddRule(PieceType.BombPiece, PieceType.RainbowPiece, PieceType.RocketBombCombination);
+            AddRule(PieceType.BombPiece, PieceType.RainbowPiece, CombinationType.RainbowBombCombination);
+            
+            AddRule(PieceType.RocketPiece, PieceType.RocketPiece, CombinationType.RocketRocketCombination);
+            
+            AddRule(PieceType.RocketPiece, PieceType.RainbowPiece, CombinationType.RainbowRocketCombination);
   
+            AddRule(PieceType.RainbowPiece, PieceType.RainbowPiece, CombinationType.RainbowRainbowCombination);
+
         }
 
-        private void AddRule(PieceType typeA, PieceType typeB, PieceType result)
+        private void AddRule(PieceType typeA, PieceType typeB, CombinationType result)
         {
             _combinationRules[(typeA, typeB)] = result;
             _combinationRules[(typeB, typeA)] = result;
         }
 
-        public bool TryGetCombinationResult(PieceType typeA, PieceType typeB, out PieceType combination)
+        public bool TryGetCombinationResult(PieceType typeA, PieceType typeB, out CombinationType combination)
         {
             return _combinationRules.TryGetValue((typeA, typeB), out combination);
         }
