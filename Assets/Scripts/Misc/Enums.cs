3755bd7877d6f8141a87b0c9f230ed1bd018a36b
Eray <eray.yasar37@hotmail.com>
2025-04-04 18:09:02 +0300

    Add new types

--- Assets/Scripts/Misc/Enums.cs
+++ Assets/Scripts/Misc/Enums.cs
@@ -7,12 +7,19 @@ namespace Misc
     public enum PieceType
     {
         None,
+
         ObstaclePiece,
         BombPiece,
         RocketPiece,
+        RainbowPiece,
+
+
+        
+        
         SquareNormalPiece = NormalPieceType.Square,
         CircleNormalPiece =  NormalPieceType.Circle,
         TriangleNormalPiece =  NormalPieceType.Triangle,
+
     }
 
     public enum NormalPieceType
@@ -20,5 +27,15 @@ namespace Misc
         Square = 100,
         Circle = 101,
         Triangle = 102,
+    }
+
+    public enum CombinationType
+    {
+        RocketBombCombination,
+        BombBombCombination,
+        RocketRocketCombination,
+        RainbowRocketCombination,
+        RainbowBombCombination,
+        RainbowRainbowCombination,
     }
 }
